a8d57cee44cbf07fe39b99896a7cf00f371a7586
armejbha <abdurrahim181878@gmil.com>
2025-07-16 23:37:18 +0600

    update

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ async function run() {
 
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
 
@@ -815,8 +815,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (error) {
         console.error("Connection Failed", error)
     }
